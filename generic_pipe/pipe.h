32c815c9ba619307dd28913bd2895c14111c60c4
michal.andrasiak <michal.andrasiak@gmail.com>
2022-03-04 17:16:30 +0100

    Add pipe implementation

---
+++ generic_pipe/pipe.h
@@ -0,0 +1,27 @@
+#ifndef PIPE_H
+#define PIPE_H
+
+#include <utility>
+
+template <typename L, typename R> class pipe {
+public:
+  constexpr pipe(L lhs, R rhs) : lhs_{lhs}, rhs_{rhs} {}
+
+  template <typename... T> constexpr auto operator()(T &&...args) const {
+    return call_rhs(lhs_(std::forward<T>(args)...));
+  }
+
+private:
+  L lhs_;
+  R rhs_;
+
+  template <typename... T> constexpr auto call_rhs(T &&...args) const {
+    return rhs_(std::forward<T>(args)...);
+  }
+};
+
+template <typename L, typename R> constexpr auto operator|(L &&lhs, R &&rhs) {
+  return pipe<L, R>(std::forward<L>(lhs), std::forward<R>(rhs));
+}
+
+#endif

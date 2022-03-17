521c9e02dd609050ebebc3da17b8f08004390cc3
michal.andrasiak <michal.andrasiak@gmail.com>
2022-03-17 15:27:43 +0100

    Add static_string readme and kind of use case

--- static_string/static_string.h
+++ static_string/static_string.h
@@ -22,4 +22,47 @@ template<size_t Size> struct static_string
 
 template<size_t N> static_string(const char (&data)[N]) -> static_string<N>;
 
+template<static_string Name, typename T> struct named_counter
+{
+    using type = T;
+    constexpr static_string name = Name;
+
+    constexpr std::string_view name() const noexcept
+    {
+        return Name.get();
+    }
+
+    constexpr void inc() noexcept
+    {
+        count++;
+    }
+    int count = 0;
+};
+
+template<typename... T> struct counter : T...
+{
+public:
+    constexpr counter() { }
+
+    template<static_string Name> constexpr int inc() noexcept
+    {
+        return inc_impl_name<Name>(*this);
+    }
+
+    template<typename Type> constexpr int inc() noexcept
+    {
+        return inc_impl_type<Type>(*this);
+    }
+
+private:
+    template<static_string Name, typename U> constexpr int inc_impl_name(named_counter<Name, U>& n)
+    {
+        return n.inc();
+    }
+
+    template<typename U, static_string Name> constexpr int inc_impl_type(named_counter<Name, U>& n)
+    {
+        return n.inc();
+    }
+};
 #endif
